235fa9f2813efb8643adf582af4c0f43d78d48a5
Owen Mecham <owen.mecham@adesa.com>
2020-11-15 23:23:00 -0700

    webpack config done app-shell

---
+++ projects/app-shell/webpack.config.js
@@ -0,0 +1,17 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+  output: {
+    publicPath: "http://localhost:5000/",
+    uniqueName: "shell"
+  },
+  optimization: {
+    // Only needed to bypass a temporary bug
+    runtimeChunk: false
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      shared: ["@angular/core", "@angular/common", "@angular/router"]
+    })
+  ],
+};
