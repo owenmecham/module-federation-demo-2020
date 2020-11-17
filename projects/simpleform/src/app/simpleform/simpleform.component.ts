5529b8df456cbf7c5125733e90f571fdd82b1a34
owenmecham <owen.mecham@gmail.com>
2020-11-16 22:55:07 -0700

    working form module

--- projects/simpleform/src/app/simpleform/simpleform.component.ts
+++ projects/simpleform/src/app/simpleform/simpleform.component.ts
@@ -1,12 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-simpleform',
   templateUrl: './simpleform.component.html',
   styleUrls: ['./simpleform.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom,
 })
-export class SimpleFormComponent implements OnInit {
-  constructor() {}
+export class SimpleFormComponent {
+  powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
 
-  ngOnInit(): void {}
+  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+  }
+
+  // TODO: Remove this when we're done
+  get diagnostic() {
+    return JSON.stringify(this.model);
+  }
+
+  newHero() {
+    this.model = new Hero(42, '', '');
+  }
+
+  skyDog(): Hero {
+    const myHero = new Hero(
+      42,
+      'SkyDog',
+      'Fetch any object at any distance',
+      'Leslie Rollover'
+    );
+    console.log('My hero is called ' + myHero.name); // "My hero is called SkyDog"
+    return myHero;
+  }
+
+  //////// NOT SHOWN IN DOCS ////////
+
+  // Reveal in html:
+  //   Name via form.controls = {{showFormControls(heroForm)}}
+  showFormControls(form: any) {
+    return form && form.controls.name && form.controls.name.value; // Dr. IQ
+  }
+
+  /////////////////////////////
 }
--- projects/simpleform/src/app/simpleform/simpleform.module.ts
+++ projects/simpleform/src/app/simpleform/simpleform.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { SimpleFormRoutingModule } from './simpleform-routing.module';
 import { SimpleFormComponent } from './simpleform.component';
 
 @NgModule({
   declarations: [SimpleFormComponent],
-  imports: [CommonModule, SimpleFormRoutingModule],
+  imports: [CommonModule, SimpleFormRoutingModule, FormsModule],
 })
 export class SimpleFormModule {}
